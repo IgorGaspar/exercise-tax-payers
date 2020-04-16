a769e654b2c89100e1e1f17bf25ebc51ae3c7b24
Igor Gaspar <igor.elvis@hotmail.com>
2020-04-16 19:59:53 -0300

    Project complete

---
+++ ExerciseTaxPayers/Entities/Company.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseTaxPayers.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company()
+        {
+        }
+
+        public Company(string name, double anualIncome, int numberOfEmployees)
+            : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            if (NumberOfEmployees > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
